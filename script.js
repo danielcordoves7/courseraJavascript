2826e65aff049a8cab815ce78c73196111664e46
danielcordoves7 <danielcordoves72@gmail.com>
2025-03-09 11:33:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function unDo(){
     document.getElementById('message').innerHTML = "Hover over an image to display the alt text.";
     document.getElementById('message').style = `background-image:none`;
 }
+function setImages(){
+    currentImages = document.querySelectorAll(".preview");
+    for(let i = 0; i<currentImages.length;i++){
+        console.log(i);
+        currentImages[i].setAttribute("tabindex","0");
+        currentImages[i].addEventListener("focus",function(){upDate(currentImages[i])});
+    }
+}
